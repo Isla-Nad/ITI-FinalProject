3ccfad75abae73106a57829563137c484bc10c6f
minaa <monos_102000@yahoo.com>
2023-09-17 13:38:49 +0200

    change in footer and container and nav

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+import Container from "./Container";
+import Footer from "./Footer";
+
+function Home (){
+<>
+<Nav/>
+<Container/>
+<Footer/>
+</>
+}
+export default Home;
