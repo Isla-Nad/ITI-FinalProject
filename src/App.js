b5ea432adbb3883b01bc13fbc9e981f2a836cbb3
minaa <monos_102000@yahoo.com>
2023-09-20 19:46:34 +0200

    all change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home/Home';
+import Nav from './Home/Nav';
+import Footer from './Home/Footer';
+import Container from './Home/Container';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav/>
+    <Container/>
+    <Footer/>
+
+    </>
   );
 }
 
--- src/Home/Container.js
+++ src/Home/Container.js
@@ -1,6 +1,18 @@
+import image from "./Banner.png"
 function Container(){
-return 
+return (
 <>
+<div className="container-image">
+    <img src={image} style={{width:"100%"}}/>
+</div>
+<div className="banner-text">
+    <h1>Welcome To Dentos</h1>
+    <h3>Every Smiles Everday</h3>
+    <p>--because your smile is our passion</p>
+   
+</div>
+
 </>
+)
 }
 export default Container;
