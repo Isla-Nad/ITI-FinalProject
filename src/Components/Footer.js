8cac39ca012b784200f6815a02b3e9ad15ec3741
Islam Nady <islamnady95@gmail.com>
2023-09-21 17:35:55 +0300

    adding react-icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,15 @@
+import { FaPhoneAlt, FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
 function Footer() {
   return (
     <div className="footer">
       <div className="text text-wite ">
-        <i className="fa-solid fa-phone text-white"></i> <h className="text-white">:355462236</h>
+        <FaPhoneAlt className="text-white" /> <h className="text-white">:355462236</h>
       </div>
       <div className="icons">
-        <i className="fa-brands fa-facebook fa-xl text-white"></i>
-        <i className="fa-brands fa-instagram fa-xl text-white" style={{ paddingLeft: "10px" }}></i>
-        <i className="fa-brands fa-twitter fa-xl" style={{ color: "white", paddingLeft: "10px" }}></i>
+        <FaFacebookF className="text-white mx-2" />
+        <FaInstagram className="text-white mx-2" />
+        <FaTwitter className="text-white mx-2" />
       </div>
       <p className="text-white" style={{ textAlign: "center", paddingTop: "50px" }}>
         copyright@iti_Dentos
