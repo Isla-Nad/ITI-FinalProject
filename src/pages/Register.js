b3f390b71399033a50128ca94bde8a2e4a37df70
abdlerhmang <abdogooda320@gmail.com>
2023-09-28 00:43:50 +0300

    registeration upadates

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Error from '../components/error';
-
+import './register.css';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -26,13 +26,13 @@ const Register = () => {
     const emailPattern = /^\S+@\S+\.\S+$/;
     if(!emailPattern.test(formData.email))
     {
-      formErrors.email = 'Invalid Email Adress'
+      formErrors.email = 'Wrong or Invalid email address. Please correct and try again.'
     }
 
     const passwordPattern = /^.{8,}$/;
     if(!passwordPattern.test(formData.password))
     {
-      formErrors.password = 'Invalid password'
+      formErrors.password = 'Minimum 8 characters required'
     }
 
     // confirm password validation 
@@ -46,20 +46,68 @@ const Register = () => {
     e.preventDefault();
     // test the 
     setErrors(validateForm())
-    // console.log(validateForm)
-    console.log(errors)
+    const errorsValues =Object.values(errors) ;
+    console.log(errorsValues)
+    // if (!errorsKeys === 0) {
+    //   console.log("NOooo Erorrsssss");
+    // }
+    // else{
+    //   console.log('erroreoreeoreor')
+    // }
   };
 
+    // Use useEffect to run code after the state update
+  useEffect(() => {
+    // Now you can safely access the updated errors state
+    const errorsValues = Object.values(errors);
+    // console.log(errorsValues);
+    // Check if there are errors and handle accordingly
+    if (errorsValues.length === 0) {
+      localStorage.setItem('userRegistration', JSON.stringify(formData));
+    } else {
+      console.log("There are errors");
+    }
+  }, [errors]); // This will run whenever the errors state change
+
+
+  /// register as a doctor or a Patient
+  const [role, setRole] = useState('patient'); // Default to 'patient'
+
+  const handleRoleChange = (e) => {
+    setRole(e.target.value);
+  };
+  
   return (
     <div className="container mt-5 mb-5">
         <div className="row justify-content-center">
-        <div className="col-md-6">
-            <div className="">
-            <div className="">
+        <div className="col-md-6 card custom-card">
                 <h2 className="text-center">Register</h2>
                 <form onSubmit={handleSubmit}  noValidate>
                 <div className="form-group">
-                    <label htmlFor="name">Name</label>
+                      <label>
+                        <input
+                          type="radio"
+                          value="patient"
+                          className='form-check-input'
+                          checked={role === 'patient'}
+                          onChange={handleRoleChange}
+                        />
+                        Patient
+                      </label>
+                      <span>&nbsp; &nbsp; &nbsp;</span>
+                      <label>
+                        <input
+                          type="radio"
+                          value="doctor"
+                          className='form-check-input'
+                          checked={role === 'doctor'}
+                          onChange={handleRoleChange}
+                        />
+                        Doctor
+                      </label>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
                     <input
                     type="text"
                     className="form-control"
@@ -67,11 +115,12 @@ const Register = () => {
                     name="name"
                     value={formData.name}
                     onChange={handleChange}
+                    placeholder="Your Name"
                     required
                   />
                 </div>
                 <div className="form-group">
-                  <label htmlFor="email">Email</label>
+                  <label htmlFor="email">Email: </label>
                   <input
                     type="email"
                     className="form-control"
@@ -79,12 +128,28 @@ const Register = () => {
                     name="email"
                     value={formData.email}
                     onChange={handleChange}
+                    placeholder="you@example.com"
                     required
                   />
                 </div>
                 {errors && <Error error={errors.email} />}
+                {role === 'doctor' && (
+                  <div className="form-group">
+                  <label htmlFor="email">Clinic Name: </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Clinic name"
+                    required
+                  />
+                  </div>
+                )}
                 <div className="form-group">
-                  <label htmlFor="password">Password</label>
+                  <label htmlFor="password">Password:</label>
                   <input
                     type="password"
                     className="form-control"
@@ -92,12 +157,13 @@ const Register = () => {
                     name="password"
                     value={formData.password}
                     onChange={handleChange}
+                    placeholder="Password"
                     required
                   />
                   {errors && <Error error={errors.password} />}
                 </div>
                 <div className="form-group">
-                  <label htmlFor="confirmPassword">Confirm Password</label>
+                  <label htmlFor="confirmPassword">Confirm Password:</label>
                   <input
                     type="password"
                     className="form-control"
@@ -105,16 +171,17 @@ const Register = () => {
                     name="confirmPassword"
                     value={formData.confirmPassword}
                     onChange={handleChange}
+                    placeholder="Confirm Password"
                     required
                   />
                   {errors && <Error error={errors.confirmPassword} />}
                 </div>
+                <div class="d-flex justify-content-center align-items-center">
                 <button type="submit" className="btn btn-primary btn-block">
-                  Register
+                  Create Account
                 </button>
+                </div>
               </form>
-            </div>
-          </div>
         </div>
       </div>
     </div>
