35fc03b620f21b540ad600788e18417f5ef8c1e0
Islam Nady <islamnady95@gmail.com>
2023-10-09 13:38:53 +0300

    latest merge

--- src/pages/DoctorProfile/DoctorProfile.js
+++ src/pages/DoctorProfile/DoctorProfile.js
@@ -1,44 +1,26 @@
 import axios from "axios";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserNurse, faAddressCard, faCertificate, faBriefcaseMedical, faPhone, faBookMedical, faStar, faPen} from "@fortawesome/free-solid-svg-icons";
+import { faUserNurse, faAddressCard, faCertificate, faBriefcaseMedical, faPhone, faBookMedical, faStar, faPen } from "@fortawesome/free-solid-svg-icons";
 import "./DoctorProfile.css";
 import { Link, useParams } from "react-router-dom";
 import AppointmentPicker from "./appointments/AppointmentPicker";
 import CommentsAndRating from "./CommentsAndRating";
 
 function DentistProfile() {
-  
-  const {id} = useParams();
-  const [DentData,setDentData] = useState({})
-  useEffect (()=> {
-    axios.get("https://api-generator.retool.com/9WmJCF/ddata/"+id)
-    .then(res => setDentData(res.data))
-    .catch(err => console.log(err))
-    
-
-  },[])
-  console.log(DentData)
+  const { id } = useParams();
+  const [DentData, setDentData] = useState({});
+  useEffect(() => {
+    axios
+      .get("https://api-generator.retool.com/9WmJCF/ddata/" + id)
+      .then((res) => setDentData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(DentData);
 
   // const [Mydentist,setMydentist] = useState()
   // setMydentist(DentData[0])
   // console.log(Mydentist)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return (
     <>
@@ -94,17 +76,18 @@ function DentistProfile() {
           <div id="profile--container">
             <div data-bs-spy="scroll" data-bs-target="#sidebar-nav-1" data-bs-smooth-scroll="true" className="scrollspy-example" tabIndex="0">
               <div id="profile">
-                <h2 className="text-primary" style={{fontSize:"4rem"}}>{DentData.FirstName} {DentData.LastName}</h2>
+                <h2 className="text-primary" style={{ fontSize: "4rem" }}>
+                  {DentData.FirstName} {DentData.LastName}
+                </h2>
                 <p>{DentData.dentPosition}</p>
-                <Link className="text-dark" to={`/update/${DentData.id}`}><FontAwesomeIcon icon={faPen} size="lg" /></Link>
-                
+                <Link className="text-dark" to={`/update/${DentData.id}`}>
+                  <FontAwesomeIcon icon={faPen} size="lg" />
+                </Link>
               </div>
               <div id="About" className="mt-5">
                 <h2 className="text-primary">About The Dentist</h2>
                 <hr />
-                <p>
-                  {DentData.dentDescription}
-                </p>
+                <p>{DentData.dentDescription}</p>
               </div>
               <div id="Certificates" className="mt-5">
                 <h2 className="text-primary">Certificates</h2>
@@ -154,10 +137,9 @@ function DentistProfile() {
                   {/* <FontAwesomeIcon icon={faWhatsapp} size="2xl" /> */}
                   {/* <FontAwesomeIcon icon={faFacebook} size="2xl" /> */}
                   {/* <FontAwesomeIcon icon={faInstagram} size="2xl" /> */}
-
                 </p>
               </div>
-              
+
               <div id="Contacts" className="mt-5">
                 <h2 className="text-primary">Appointment Booking</h2>
                 <hr />
